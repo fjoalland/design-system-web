f9af6d50f6e37468327ce4d2a73732a754d789ca
Julien BAYLE <julien.bayle@loire-atlantique.fr>
2019-08-22 16:17:47 +0200

    Fix: Browsersync did reload only once

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 Le design system est divisé en deux parties : la documentation et le framework CSS/JS.
 
 La documentation s'appuie sur un site Jekyll avec un outillage en Ruby.
-Elle est disponible à cette adresse : https://departement-loire-atlantique.github.io/design-system-web/
+Elle est disponible à cette adresse : https://design.loire-atlantique.fr
 
 Le framework s'appuie sur SCSS avec un outillage en NodeJS.
 
@@ -14,19 +14,19 @@ Récupérer le code du projet : `git clone https://github.com/departement-loire-
 
 ### Contribuer à la documentation
 
-#### Version manuelle
+#### Solution 1 : Installation des outils en local
 
 1. Installer [Ruby](https://www.ruby-lang.org/en/documentation/installation/)
 2. Dans une console : installer Ruby bundler : `gem install bundler`
 3. Dans une console, installer les dépendances : `cd docs && bundle install` (ou `bundle update` pour une mise à jour)
-4. Dans une console, lancer le mode développement : `cd docs && bundle exec jekyll serve --incremental` 
-5. Ouvrir le navigateur à l'addresse `http://localhost:4000/design-system-web/` pour voir le site (mise à jour automatique au fur et à mesure de l'édition des fichiers).
+4. Dans une console, lancer le mode développement : `cd docs && bundle exec jekyll serve --watch` 
+5. Ouvrir le navigateur à l'addresse `http://localhost:4000/` pour voir le site (mise à jour automatique au fur et à mesure de l'édition des fichiers).
 
-#### Version avec Docker
+#### Solution 2 : Utilisation de Docker
 
 1. Installer Docker et [Docker compose](https://docs.docker.com/compose/install/) 
 2. Pour démarrer le projet : `cd .docker && docker-compose up`
-3. Ouvrez un navigateur à l'adresse : `http://localhost:4000/design-system-web/`
+3. Ouvrez un navigateur à l'adresse : `http://localhost:4000/` pour voir le site (mise à jour automatique au fur et à mesure de l'édition des fichiers).
 
 Si le port 4000 est déja utilisé sur votre machine, vous pouvez le modifier en modifiant la ligne `PORT:4000` du fichier `.docker/.env`
 
@@ -58,7 +58,7 @@ Le framework dérive de BootStrap et KNACSS
 
 1. Installer [NodeJS](https://nodejs.org/en/download/)
 2. Dans une console, installer les dépendances du projet : `npm install`
-3. Compiler le projet : `gulp build`
+3. Compiler le projet : `gulp build:ds`
 
 Description de l'arborescence du framework (sous dossier framework) :
 
@@ -70,16 +70,28 @@ Description de l'arborescence du framework (sous dossier framework) :
 * gulpfile.js : Tâches de compilation et de développemnet
 * package.json : Dépendances NodeJS pour les tâches GULP
 
-### Contribuer de manière avancée
+### Contribuer simultanément à la documentation et au framework scss/js
 
-Pour modifier ou ajouter un composant, il est possible d'activer un mode de développement.
-Ce mode permet de recharger automatiquement le navigateur en temps réel suite à toute modification dans la documentation ou le framework. 
+Ce mode permet de recharger automatiquement le navigateur en temps réel suite à toute modification dans la documentation ou le framework.
 
-Simplement lancer `gulp serve` et ouvrir un http://localhost:3000
+Installer les outils conformément aux étapes ci-dessus.
 
-Puis, aller sur le composant ou la page que l'on souhaite modifier.
+Puis simplement lancer `gulp serve` et ouvrir un http://localhost:4000
+
+Puis, aller sur le composant ou la page que l'on souhaite modifier. 
 La mise à jour d'une modification dans le SCSS du framework est immédiate sans rechargement de la page.
-La mise à jour de la documentation ou du gabarit HTML quelques secondes à s'afficher.
+La mise à jour d'un fichier JS du framework est prise en compte en environ 500ms avec rechargement de la page.
+La mise à jour d'un des fichiers de la documentation est prise en compte en 2 à 3 secondes avec rechargement de la page.
+
+### Publier une nouvelle version
+
+Pour créer un tag afin de marquer une nouvelle révision du projet, utiliser simplement les commandes de gestion de version prévue dans npm selon l'importance des modifications réalisée depuis la précédente version :
+
+```bash
+npm version patch
+npm version minor
+npm version major
+```
 
 ## License
 
--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var postcss = require('gulp-postcss');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
-var run = require('gulp-run');
+var exec = require('child_process').exec;
 var gutil = require('gulp-util');
 var browserSync = require('browser-sync').create();
 var cssnano = require('cssnano'); // minifies CSS
@@ -14,11 +14,14 @@ var flexbugs = require('postcss-flexbugs-fixes'); // flexbox fixes for IE
 var gaps = require('postcss-gap-properties'); // gaps polyfill
 
 var jekyllDir = "docs/",
-    cssDest = 'framework/css/',
-    jsFiles = ['framework/js/**/*.js', '!framework/js/*.js'],
-    jsDest  = 'framework/js';
+    scssFile = 'framework/scss/cd44.scss'
+    scssRGFile = 'framework/scss/reset_grid.scss'
+    cssDest = 'dist/css',
+    jsFiles = 'framework/js/**/*.js'
+    jsDest  = 'dist/js';
+    assetsFolders = ['framework/fonts/**', 'framework/images/**'];
 
-var postCssPlugins = [
+var postCssPluginsDev = [
   unprefix(),
   autoprefixer({
     grid: true
@@ -38,29 +41,30 @@ var postCssPluginsProd =  [
 ];
 
 gulp.task('build:css:cd44:dev', function () {
-  return gulp.src('framework/scss/cd44.scss')
+  return gulp.src(scssFile)
     .pipe(sass({
-      outputStyle: 'expanded' // CSS non minifiée plus lisible ('}' à la ligne)
+      // CSS non minifiée plus lisible ('}' à la ligne)
+      outputStyle: 'expanded' 
     }))
-    .pipe(postcss(postCssPlugins))
+    .pipe(postcss(postCssPluginsDev))
     .pipe(rename('cd44.css'))
     .pipe(browserSync.stream())
     .pipe(gulp.dest(cssDest));
 });
 
-gulp.task('build:css:ResetAndGrid', function () {
-  return gulp.src('framework/scss/reset_grid.scss')
+gulp.task('build:css:cd44:prod', function () {
+    return gulp.src(scssFile)
+      .pipe(sass())
+      .pipe(postcss(postCssPluginsProd))
+      .pipe(rename('cd44.min.css'))
+      .pipe(gulp.dest(cssDest));
+  });
+
+gulp.task('build:css:reset_grid', function () {
+  return gulp.src(scssRGFile)
     .pipe(sass())
     .pipe(postcss(postCssPluginsProd))
     .pipe(rename('reset_grid.min.css'))
-    .pipe(gulp.dest(cssDest));
-});
-
-gulp.task('build:css:cd44:prod', function () {
-  return gulp.src('framework/scss/cd44.scss')
-    .pipe(sass())
-    .pipe(postcss(postCssPluginsProd))
-    .pipe(rename('cd44.min.css'))
     .pipe(gulp.dest(cssDest));
 });
 
@@ -70,50 +74,54 @@ gulp.task('build:js', function() {
       .pipe(gulp.dest(jsDest));
 });
 
-gulp.task('build:jekyll', function() {
-  return gulp.src(jekyllDir)
-    .pipe(run('cd docs && bundle exec jekyll build'))
-    .on('error', gutil.log);
+gulp.task('build:jekyll', function(cb) {
+  exec('cd ' + jekyllDir + ' && bundle exec jekyll build --incremental', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
 });
 
-gulp.task('build', gulp.parallel('build:css:cd44:dev', 'build:css:cd44:prod', 'build:js', 'build:jekyll'));
+gulp.task('build:ds', gulp.parallel(
+    'build:css:cd44:dev', 
+    'build:css:cd44:prod', 
+    'build:css:reset_grid',
+    'build:js',
+    function() {
+        return gulp.src(assetsFolders, {base: 'framework'})
+                .pipe(gulp.dest('dist'));
+    })
+);
+
+gulp.task('build', gulp.parallel(
+    'build:jekyll',
+    'build:ds' 
+    ));
 
 gulp.task('serve', gulp.series('build', function() {
   
   browserSync.init({
     server: {
-      baseDir : 'docs/_site',
-      middleware: function(req,res,next) {
-        // When publihed on github pages, base path starts with 'design-system-web'
-        if (req.url.startsWith('/design-system-web')) {
-          req.url = req.url.substr(18);
-        } 
-        return next();
-      },
-      // Load design system from the framework directory
-      routes: {
-        "/build": "framework"
-      }
+      baseDir : 'dist'
     },
+    port: 4000,
     ghostMode: false, // do not mirror clicks, reloads, etc. (performance optimization)
     logFileChanges: true,
     open: false       // do not open the browser (annoying)
   });
 
-  gulp.watch('docs/**/*.(md|yml|js|scss)', gulp.series('build:jekyll', function() {
+  gulp.watch('docs/**/*', gulp.series('build:jekyll', function(done) {
     browserSync.reload(); 
-  }));
-
-   gulp.watch('docs/_variation/**/*.html', gulp.series('build:jekyll', function() {
-    browserSync.reload();
+    done();
   }));
 
   // Watch framework .scss files
   gulp.watch(['framework/scss/**/*.scss'], gulp.series('build:css:cd44:dev'));
 
   // Watch framework .js files
-  gulp.watch('framework/js/**/*.js', gulp.series('build:js', function() {
+  gulp.watch('framework/js/**/*.js', gulp.series('build:js', function(done) {
     browserSync.reload(); 
+    done();
   }));
 
 }));
