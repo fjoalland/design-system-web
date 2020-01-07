1e64c541a4666bb3bca04faba0e568602cf0a6a5
camille-david <57793321+camille-david@users.noreply.github.com>
2020-01-07 17:54:18 +0100

    Carrousel (#48)
    * fix : bug affichage fleche next en petit ecran
    
    * Update fonctionnement carrousel + insertion card et carrousel

--- framework/js/components/ds44-initCarrousel.js
+++ framework/js/components/ds44-initCarrousel.js
@@ -8,14 +8,17 @@ for(let element of allCarousel) {
   const nextEl = element.getElementsByClassName("swiper-button-next")[0];
   const prevEl = element.getElementsByClassName("swiper-button-prev")[0];
   const swiper = element.getElementsByClassName("swiper-container")[0];
-  const arrSlide = swiper.getElementsByClassName("swiper-slide")
+  const arrSlide = swiper.getElementsByClassName("swiper-slide");
   const nbrSlide = arrSlide.length;
   const wrapper = element.getElementsByClassName("swiper-wrapper")[0];
   const nbrMaxSlide = wrapper.classList.contains("grid-3-small-1") ? 3 : 4;
 
+  const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
   var swiperObj = new Swiper (swiper, {
     direction: 'horizontal',
     spaceBetween: 10,
+    watchOverflow: true,
     navigation: {
       nextEl: nextEl,
       prevEl: prevEl,
@@ -24,128 +27,87 @@ for(let element of allCarousel) {
       firstSlideMessage: 'Ceci est le premier contenu',
       lastSlideMessage: 'Ceci est le dernier contenu',
     },
-    breakpoints: {
-      576: {
-        loop: nbrSlide > 1,
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: nbrMaxSlide,
-      },
-    }
+    slidesPerView: screenWidth >= 768 ? nbrMaxSlide : 1,
+    loop: screenWidth >= 768 ? (nbrSlide > nbrMaxSlide) : (nbrSlide > 1),
   });
 
   allSwiper.push(swiperObj);
 
-  const prevSlideMessageStr = 'Voir le contenu précédent';
-  const nextSlideMessageStr = 'Voir le contenu suivant';
+  if(swiperObj.params.loop) {
 
-  var parent = element;
-  var title = null;
-  while(parent) {
-    for(var sibling of getSiblings(parent)){
-      if(sibling && sibling.classList.contains("h2-like")) {
-        title = sibling;
-        break;
-      }
+    nextEl.classList.remove("swiper-button-disabled");
+    prevEl.classList.remove("swiper-button-disabled");
+
+    var parent = element;
+    var title = null;
+    while(parent) {
+      for(var sibling of getSiblings(parent)){
+        if(sibling && sibling.classList.contains("h2-like")) {
+          title = sibling;
+          break;
+        }
+      };
+      parent = parent.parentElement;
+    }
+    const titleCarrousel = title ? title.innerText : "Carrousel n°"+i;
+
+    disableAllTabIndexes(element);
+    prevEl.setAttribute("tabindex", "1");
+    nextEl.setAttribute("tabindex", "3");
+
+    const prevSlideMessageStr = 'Voir le contenu précédent';
+    const nextSlideMessageStr = 'Voir le contenu suivant';
+
+    var updatePrevAndNextSlideMessage = function() {
+      prevEl.setAttribute("title", prevSlideMessageStr+" "+titleCarrousel+" - "+(swiperObj.realIndex+1)+"/"+nbrSlide);
+      nextEl.setAttribute("title", nextSlideMessageStr+" "+titleCarrousel+" - "+(swiperObj.realIndex+1)+"/"+nbrSlide);
     };
-    parent = parent.parentElement;
-  }
-  const titleCarrousel = title ? title.innerText : "Carrousel n°"+i;
 
-  var indexTuileActive = 0;
+    updatePrevAndNextSlideMessage();
 
-  disableAllTabIndexes(element);
-  prevEl.setAttribute("tabindex", "1");
-  nextEl.setAttribute("tabindex", "3");
+    var titreTuileActive = arrSlide[swiperObj.activeIndex].querySelector(".ds44-card__title a[href]:not([disabled])");
+    titreTuileActive.setAttribute("tabindex", "2");
 
-  var updatePrevAndNextSlideMessage = function() {
-    prevEl.setAttribute("title", prevSlideMessageStr+" "+titleCarrousel+" - "+(indexTuileActive+1)+"/"+nbrSlide);
-    nextEl.setAttribute("title", nextSlideMessageStr+" "+titleCarrousel+" - "+(indexTuileActive+1)+"/"+nbrSlide);
-  };
+    for(var button of [nextEl, prevEl]) {
+      button.addEventListener("click", function(event) {
+        titreTuileActive.setAttribute("tabindex", "-1");
+        titreTuileActive = arrSlide[swiperObj.activeIndex].querySelector(".ds44-card__title a[href]:not([disabled])");
+        titreTuileActive.setAttribute("tabindex", "2");
 
-  updatePrevAndNextSlideMessage();
+        setTimeout(function(){ //pour que la methode ait lieu apres swipper
+          updatePrevAndNextSlideMessage();
+        }, 5);
+        setTimeout(function(){ //pour que la methode ait lieu apres l'animation de scroll
+          titreTuileActive.focus();
+        }, 150);
+      }, false);
 
-  var titreTuileActive = arrSlide[indexTuileActive].querySelector(".ds44-card__title a[href]:not([disabled])");
-  titreTuileActive.setAttribute("tabindex", "2");
-
-  prevEl.addEventListener("click", function() {
-    if(indexTuileActive > 0) {
-      titreTuileActive.setAttribute("tabindex", "-1");
-      indexTuileActive--;
-      titreTuileActive = arrSlide[indexTuileActive].querySelector(".ds44-card__title a[href]:not([disabled])");
-      titreTuileActive.setAttribute("tabindex", "2");
-      titreTuileActive.focus();
     }
-  }, false);
 
-  nextEl.addEventListener("click", function(event) {
-    if(indexTuileActive < (nbrSlide-1)) {
-      titreTuileActive.setAttribute("tabindex", "-1");
-      indexTuileActive++;
-      titreTuileActive = arrSlide[indexTuileActive].querySelector(".ds44-card__title a[href]:not([disabled])");
-      titreTuileActive.setAttribute("tabindex", "2");
-      titreTuileActive.focus();
-    }
-  }, false);
-
-  var removeAriaDisabledFalse = function(button) {
-    if(button.getAttribute("aria-disabled") == "false") button.removeAttribute("aria-disabled");
-  }
-
-  var updateVisibilityPrevAndNext = function(){
-    if(swiperObj.realIndex == 0 && indexTuileActive > 0) {
-      //cas ou la fleche prev n'est pas visible de la meme maniere pour les utilisateurs voyants et malvoyants
-      prevEl.classList.remove("swiper-button-disabled");
-      prevEl.classList.add("swiper-button-hidden");
-    } else {
-      prevEl.classList.remove("swiper-button-hidden");
-      if(indexTuileActive == 0) {
-        prevEl.classList.add("swiper-button-disabled");
+    prevEl.addEventListener("keypress", function(event){
+      if(event.key === "Enter" || event.key === " " || event.key === "Spacebar") {
+        event.preventDefault();
+        prevEl.click();
       }
-    }
-    var realIndexMax = nbrSlide > nbrMaxSlide ? nbrSlide-nbrMaxSlide : nbrSlide-1;
-    if(swiperObj.realIndex == realIndexMax && indexTuileActive < (nbrSlide-1)) {
-      //cas ou la fleche next n'est pas visible de la meme maniere pour les utilisateurs voyants et malvoyants
-      nextEl.classList.remove("swiper-button-disabled");
-      nextEl.classList.add("swiper-button-hidden");
-    } else {
-      nextEl.classList.remove("swiper-button-hidden");
-      if(indexTuileActive == (nbrSlide-1)) {
-        nextEl.classList.add("swiper-button-disabled");
+    });
+
+    nextEl.addEventListener("keypress", function(event){
+      if(event.key === "Enter" || event.key === " " || event.key === "Spacebar") {
+        event.preventDefault();
+        nextEl.click();
       }
+    });
+
+  } else {
+
+    disableAllTabIndexes(element);
+    var index = 1;
+    for(var slide of arrSlide) {
+    slide.querySelector(".ds44-card__title a[href]:not([disabled])").setAttribute("tabindex", index);
+      index++;
     }
-  }
-
-  const allButtonElement = [nextEl, prevEl];
-  for(var button of allButtonElement) {
-    removeAriaDisabledFalse(button);
-    button.addEventListener("click", function() {
-      setTimeout(function(){ //pour que la methode ait lieu apres swipper
-        allButtonElement.forEach(button2 => removeAriaDisabledFalse(button2));
-
-        updateVisibilityPrevAndNext();
-
-        updatePrevAndNextSlideMessage();
-      }, 5);
-    }, false);
-
 
   }
-
-  prevEl.addEventListener("keypress", function(event){
-    if(event.key === "Enter" || event.key === " " || event.key === "Spacebar") {
-      event.preventDefault();
-      prevEl.click();
-    }
-  });
-
-  nextEl.addEventListener("keypress", function(event){
-    if(event.key === "Enter" || event.key === " " || event.key === "Spacebar") {
-      event.preventDefault();
-      nextEl.click();
-    }
-  });
 
 }
 
