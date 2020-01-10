6142c703e8039cdd781a55ca388d1766a7ed7c9d
Vila Cornellas <jordi.vila@trsb.net>
2020-01-10 09:52:38 +0100

    Evol [Header et Breadcrumb] - Inclusion du header - Inclusion du fil d'Ariane - Modifications sur le fil d'Ariane - Modification du script JS du sticky header

--- framework/js/components/ds44-headerAnim.js
+++ framework/js/components/ds44-headerAnim.js
@@ -2,29 +2,7 @@
 (function (window) {
 	function ds44_headerAnim() {
 		var _ds44_headerAnim = {};
-
-		// Génère dynamiquement la largeur du bandeau en fonction de la page
-	    _ds44_headerAnim.refreshBandeauWidth = function() {
-	    	var bandeau = document.querySelector(".ds44-blocBandeau");
-	    	if (bandeau == undefined) return;
-	    	if (window.innerWidth <= 576) {
-	    		bandeau.style.width = 'calc(100% - 2rem)';
-	    		return;
-	    	}
-	    	var referenceComponent = document.querySelector("main .ds44-container-fluid");
-	    	var referenceStyle = window.getComputedStyle(referenceComponent);
-	    	var padding = parseFloat(referenceStyle.paddingLeft) + parseFloat(referenceStyle.paddingRight);
-	    	bandeau.style.width = (referenceComponent.offsetWidth - padding) + "px";
-	    }
-
-	    // Génère dynamiquement le padding-top du body en fonction de la hauteur du bandeau
-	    _ds44_headerAnim.refreshBodyPadding = function() {
-			var bandeau = document.querySelector(".ds44-blocBandeau");
-			if (bandeau == undefined) return;
-			var body = document.querySelector("body");
-			body.style.paddingTop = (bandeau.offsetHeight) + "px";
-	    }
-
+	
 	    // Sur le focus au clavier d'un élément caché sous le header,
 	    //effectuer un scroll vers le haut pour que l'élément soit affiché
 	    _ds44_headerAnim.checkFocusPosition= function() {
@@ -94,18 +72,8 @@
 })(window);
 
 if (document.querySelector(".ds44-blocBandeau") != undefined) {
-ds44_headerAnim.checkFocusPosition();
-
-function performAllRefreshes() {
-	ds44_headerAnim.refreshBandeauWidth();
-	ds44_headerAnim.refreshBodyPadding();
-}
-
-performAllRefreshes();
-
-window.onresize = performAllRefreshes;
-
-ds44_headerAnim.enableScrollActions();
+	ds44_headerAnim.checkFocusPosition();
+	ds44_headerAnim.enableScrollActions();
 }
 
 
