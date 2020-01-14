48f3908f17522d1d50b597419ff4e09f1a206add
DAVID Camille <camille.david@loire-atlantique.fr>
2020-01-14 17:03:03 +0100

    REFACTO code js carousel.js vers component oriented

--- framework/js/components/carrousel.js
+++ framework/js/components/carrousel.js
@@ -1,112 +1,132 @@
-const allCarrousel = document.querySelectorAll(".swipper-carousel-wrap");
-let allSwiper = [];
+'use strict';
 
-if(allCarrousel) {
-  allCarrousel.forEach((element, index) => {
+class Carrousel {
 
-    const nextEl = element.getElementsByClassName("swiper-button-next")[0];
-    const prevEl = element.getElementsByClassName("swiper-button-prev")[0];
-    const swiper = element.getElementsByClassName("swiper-container")[0];
-    const arrSlide = swiper.getElementsByClassName("swiper-slide");
-    const nbrSlide = arrSlide.length;
-    const wrapper = element.getElementsByClassName("swiper-wrapper")[0];
-    const nbrMaxSlide = wrapper.classList.contains("grid-3-small-1") ? 3 : 4;
+  prevSlideMessageStr = 'Voir le contenu précédent';
+  nextSlideMessageStr = 'Voir le contenu suivant';
 
-    const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  constructor() {
+    const allCarrousel = document.querySelectorAll(".swipper-carousel-wrap");
+    let allSwiper = [];
 
-    var swiperObj = new Swiper (swiper, {
-      direction: 'horizontal',
-      spaceBetween: 10,
-      watchOverflow: true,
-      navigation: {
-        nextEl: nextEl,
-        prevEl: prevEl,
-      },
-      a11y: {
-        firstSlideMessage: 'Ceci est le premier contenu',
-        lastSlideMessage: 'Ceci est le dernier contenu',
-      },
-      slidesPerView: screenWidth >= 768 ? nbrMaxSlide : 1,
-      loop: screenWidth >= 768 ? (nbrSlide > nbrMaxSlide) : (nbrSlide > 1),
-    });
+    if(allCarrousel) {
+      allCarrousel.forEach((element, index) => {
 
-    allSwiper.push(swiperObj);
+        const nextEl = element.getElementsByClassName("swiper-button-next")[0];
+        const prevEl = element.getElementsByClassName("swiper-button-prev")[0];
+        const swiper = element.getElementsByClassName("swiper-container")[0];
+        const arrSlide = swiper.getElementsByClassName("swiper-slide");
+        const nbrSlide = arrSlide.length;
+        const wrapper = element.getElementsByClassName("swiper-wrapper")[0];
+        const nbrMaxSlide = wrapper.classList.contains("grid-3-small-1") ? 3 : 4;
 
-    if(swiperObj.params.loop) {
+        const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-      nextEl.classList.remove("swiper-button-disabled");
-      prevEl.classList.remove("swiper-button-disabled");
+        var swiperObj = new Swiper (swiper, {
+          direction: 'horizontal',
+          spaceBetween: 10,
+          watchOverflow: true,
+          navigation: {
+            nextEl: nextEl,
+            prevEl: prevEl,
+          },
+          slidesPerView: screenWidth >= 768 ? nbrMaxSlide : 1,
+          loop: screenWidth >= 768 ? (nbrSlide > nbrMaxSlide) : (nbrSlide > 1),
+        });
 
-      var parent = element;
-      var title = null;
-      while(parent) {
-        for(var sibling of getSiblings(parent)){
-          if(sibling && sibling.classList.contains("h2-like")) {
-            title = sibling;
-            break;
-          }
-        };
-        parent = parent.parentElement;
-      }
-      const titleCarrousel = title ? title.innerText : "Carrousel n°"+(index+1);
+        allSwiper.push(swiperObj);
 
-      disableAllTabIndexes(element);
-      prevEl.setAttribute("tabindex", "1");
-      nextEl.setAttribute("tabindex", "3");
+        if(swiperObj.params.loop) {
 
-      const prevSlideMessageStr = 'Voir le contenu précédent';
-      const nextSlideMessageStr = 'Voir le contenu suivant';
+          nextEl.classList.remove("swiper-button-disabled");
+          prevEl.classList.remove("swiper-button-disabled");
 
-      var updatePrevAndNextSlideMessage = () => {
-        prevEl.setAttribute("title", prevSlideMessageStr+" "+titleCarrousel+" - "+(swiperObj.realIndex+1)+"/"+nbrSlide);
-        nextEl.setAttribute("title", nextSlideMessageStr+" "+titleCarrousel+" - "+(swiperObj.realIndex+1)+"/"+nbrSlide);
-      };
+          disableAllTabIndexes(element);
+          prevEl.setAttribute("tabindex", "1");
+          nextEl.setAttribute("tabindex", "3");
 
-      updatePrevAndNextSlideMessage();
+          const titleCarrousel = this.getTitleCaroussel(element, index);
 
-      var titreTuileActive = arrSlide[swiperObj.activeIndex].querySelector(".ds44-card__title a[href]:not([disabled])");
-      titreTuileActive.setAttribute("tabindex", "2");
+          var updatePrevAndNextSlideMessage = () => {
+            prevEl.setAttribute("title", this.prevSlideMessageStr+" "+titleCarrousel+" - "+(swiperObj.realIndex+1)+"/"+nbrSlide);
+            nextEl.setAttribute("title", this.nextSlideMessageStr+" "+titleCarrousel+" - "+(swiperObj.realIndex+1)+"/"+nbrSlide);
+          };
 
-      for(var button of [nextEl, prevEl]) {
-        button.removeAttribute("aria-label");
-        button.removeAttribute("role");
+          updatePrevAndNextSlideMessage();
 
-        button.addEventListener("click", (event) => {
-
-          titreTuileActive.setAttribute("tabindex", "-1");
-          titreTuileActive = arrSlide[swiperObj.activeIndex].querySelector(".ds44-card__title a[href]:not([disabled])");
+          var titreTuileActive = arrSlide[swiperObj.activeIndex].querySelector(".ds44-card__title a[href]:not([disabled])");
           titreTuileActive.setAttribute("tabindex", "2");
 
-          //pour que la methode ait lieu apres swipper
-          setTimeout(updatePrevAndNextSlideMessage, 5);
-          //pour que la methode ait lieu apres l'animation de scroll
-          setTimeout(titreTuileActive.focus(), 150);
-        }, false);
+          for(var button of [nextEl, prevEl]) {
+            button.removeAttribute("aria-label");
+            button.removeAttribute("role");
 
-      }
-      prevEl.addEventListener("keypress", (event) => fusionneKeyPressedWithClicked(event));
-      nextEl.addEventListener("keypress", (event) => fusionneKeyPressedWithClicked(event));
+            button.addEventListener("click", (event) => {
 
-    } else {
+              titreTuileActive.setAttribute("tabindex", "-1");
+              titreTuileActive = arrSlide[swiperObj.activeIndex].querySelector(".ds44-card__title a[href]:not([disabled])");
+              titreTuileActive.setAttribute("tabindex", "2");
 
-      disableAllTabIndexes(element);
-      arrSlide.forEach((slide, index) => {
-        slide.querySelector(".ds44-card__title a[href]:not([disabled])").setAttribute("tabindex", index)
+              //pour que la methode ait lieu apres swipper
+              setTimeout(updatePrevAndNextSlideMessage, 5);
+              //pour que la methode ait lieu apres l'animation de scroll
+              setTimeout(titreTuileActive.focus(), 150);
+            }, false);
+
+          }
+          prevEl.addEventListener("keypress", (event) => fusionneKeyPressedWithClicked(event));
+          nextEl.addEventListener("keypress", (event) => fusionneKeyPressedWithClicked(event));
+
+        } else {
+
+          disableAllTabIndexes(element);
+          arrSlide.forEach((slide, index) => {
+            slide.querySelector(".ds44-card__title a[href]:not([disabled])").setAttribute("tabindex", index)
+          });
+
+        }
+        let spanNotif = element.getElementsByClassName("swiper-notification")[0];
+        //pour que la methode ait lieu apres swipper
+        setTimeout(spanNotif.remove(), 5);
+
       });
-
     }
-    let spanNotif = element.getElementsByClassName("swiper-notification")[0];
-    //pour que la methode ait lieu apres swipper
-    setTimeout(spanNotif.remove(), 5);
 
-  });
+    // besoin de garder les classes pour l'initialisation des caroussel avec js, donc on les enleve a la fin
+    this.removeAffichageSansJs();
+  }
+
+  getTitleCaroussel(parent, index) {
+    let title = null;
+    while(
+      parent != null &
+      title == null
+    ) {
+      for(var sibling of getSiblings(parent)) {
+        if(
+          sibling != null &&
+          sibling.classList.contains("h2-like")
+        ) {
+          title = sibling;
+          break;
+        }
+      };
+      parent = parent.parentElement;
+    }
+    return title != null ? title.innerText : "Carrousel n°"+(index+1);
+  }
+
+  // enleve les classes css de l'affichage sans js
+  removeAffichageSansJs() {
+    const allWrapper = document.getElementsByClassName("swiper-wrapper");
+    for(let wrapper of allWrapper){
+      wrapper.classList.remove("ds44-carousel-swiper");
+      wrapper.classList.remove("grid-4-small-1");
+      wrapper.classList.remove("grid-3-small-1");
+    }
+  }
+
 }
 
-// enleve les classes css de l'affichage sans js
-// besoin de garder les classes pour l'initialisation des caroussel avec js, donc on les enleve a la fin
-const allWrapper = document.getElementsByClassName("swiper-wrapper");
-for(let wrapper of allWrapper){
-  wrapper.classList.remove("ds44-carousel-swiper");
-  wrapper.classList.remove("grid-4-small-1");
-  wrapper.classList.remove("grid-3-small-1");
-}
+// Singleton
+new Carrousel();
