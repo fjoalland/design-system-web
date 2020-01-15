3fa835b5a8d4506bc318b455bf3f23e387ea1dac
DAVID Camille <camille.david@loire-atlantique.fr>
2020-01-15 17:44:05 +0100

    FIX #RS-448 navigation clavier operationnel https://refonte44.atlassian.net/browse/RS-448

--- framework/js/components/carrousel.js
+++ framework/js/components/carrousel.js
@@ -9,7 +9,7 @@ class Carrousel {
     const allCarrousel = document.querySelectorAll(".swipper-carousel-wrap");
 
     if(allCarrousel != null) {
-      allCarrousel.forEach((element, index) => {
+      allCarrousel.forEach((element, indexCarrousel) => {
 
         const nextEl = element.getElementsByClassName("swiper-button-next")[0];
         const innerTextNextEl = nextEl.getElementsByClassName("visually-hidden")[0];
@@ -19,25 +19,29 @@ class Carrousel {
         const arrSlide = swiper.getElementsByClassName("swiper-slide");
         const nbrSlide = arrSlide.length;
         const wrapper = element.getElementsByClassName("swiper-wrapper")[0];
-        const nbrMaxSlide = wrapper.classList.contains("grid-3-small-1") ? 3 : 4;
+        let nbrMaxSlide = wrapper.classList.contains("grid-3-small-1") ? 3 : 4;
 
         const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
+        nbrMaxSlide = screenWidth >= 768 ? nbrMaxSlide : 1;
+
         var swiperObj = new Swiper (swiper, {
           direction: 'horizontal',
-          spaceBetween: 10,
+          spaceBetween: screenWidth >= 768 ? 16 : 0,
           watchOverflow: true,
           navigation: {
             nextEl: nextEl,
             prevEl: prevEl,
           },
-          slidesPerView: screenWidth >= 768 ? nbrMaxSlide : 1,
+          slidesPerView: nbrMaxSlide,
           loop: screenWidth >= 768 ? (nbrSlide > nbrMaxSlide) : (nbrSlide > 1),
         });
 
         if(swiperObj.params.loop) {
 
-          const titleCarrousel = this.getTitleCaroussel(element, index);
+          let title = element.previousElementSibling.querySelector(".h2-like");
+
+          const titleCarrousel = title != null ? title.innerText : "Carrousel n°"+(indexCarrousel+1);
 
           const updatePrevAndNextSlideMessage = () => {
 
@@ -55,6 +59,18 @@ class Carrousel {
 
           updatePrevAndNextSlideMessage();
 
+          const getIndexDerniereTuileVisible = () => {
+            let nbrSlideWithDuplicated = arrSlide.length;
+
+            let index = swiperObj.activeIndex+(nbrMaxSlide-1);
+            if(swiperObj.activeIndex+(nbrMaxSlide-1) > nbrSlideWithDuplicated) {
+               index = swiperObj.activeIndex+(nbrMaxSlide-1) - nbrSlideWithDuplicated;
+            }
+            return index;
+          }
+
+          this.updateVisibiliteTuiles(arrSlide, swiperObj.activeIndex, getIndexDerniereTuileVisible());
+
           for(var button of [nextEl, prevEl]) {
             button.classList.remove("swiper-button-disabled");
             button.removeAttribute("aria-label");
@@ -70,36 +86,109 @@ class Carrousel {
 
           prevEl.addEventListener("click", (event) => {
 
-            let titreTuileActive = arrSlide[swiperObj.activeIndex].querySelector(".ds44-card__title a[href]:not([disabled])");
+            let tuileActive = arrSlide[swiperObj.activeIndex];
+            let titreTuileActive = tuileActive.querySelector(".ds44-card__title a[href]:not([disabled])");
+
+            for(let tuile of arrSlide) {
+              tuile.style.visibility = "visible";
+            }
             //pour que la methode ait lieu apres l'animation de scroll
-            setTimeout(() => { titreTuileActive.focus() }, 150);
+            setTimeout(() => {
+              this.updateVisibiliteTuiles(arrSlide, swiperObj.activeIndex, getIndexDerniereTuileVisible());
+              titreTuileActive.focus();
+            }, 200);
 
           });
 
           nextEl.addEventListener("click", (event) => {
 
-            let nbrSlideWithDuplicate = arrSlide.length;
+            let index = getIndexDerniereTuileVisible();
 
-            let index = swiperObj.activeIndex+(nbrMaxSlide-1);
-            if(swiperObj.activeIndex+(nbrMaxSlide-1) > nbrSlideWithDuplicate) {
-               index = swiperObj.activeIndex+(nbrMaxSlide-1) - nbrSlideWithDuplicate;
+            let tuileActive = arrSlide[index];
+            let titreTuileActive = tuileActive.querySelector(".ds44-card__title a[href]:not([disabled])");
+
+            for(let tuile of arrSlide) {
+              tuile.style.visibility = "visible";
             }
-
-            let titreTuileActive = arrSlide[index].querySelector(".ds44-card__title a[href]:not([disabled])");
             //pour que la methode ait lieu apres l'animation de scroll
-            setTimeout(() => { titreTuileActive.focus() }, 150);
+            setTimeout(() => {
+              this.updateVisibiliteTuiles(arrSlide, swiperObj.activeIndex, getIndexDerniereTuileVisible());
+              titreTuileActive.focus();
+            }, 200);
 
           });
 
           prevEl.addEventListener("keypress", (event) => fusionneKeyPressedWithClicked(event));
           nextEl.addEventListener("keypress", (event) => fusionneKeyPressedWithClicked(event));
 
+          prevEl.addEventListener("keydown", (event) => {
+            if (
+              event.key === "Tab" &&
+              ! event.shiftKey
+            ) {
+              event.preventDefault();
+              let titreTuileActive = arrSlide[swiperObj.activeIndex].querySelector(".ds44-card__title a[href]:not([disabled])");
+              titreTuileActive.focus();
+            }
+
+            if (
+              event.key === "Tab" &&
+              event.shiftKey
+            ) {
+              event.preventDefault();
+              let blockParentCarrousel = element.parentElement;
+              let blockBeforeCarrousel = blockParentCarrousel.previousElementSibling;
+              let allElementsFocusableBeforeCarrousel = blockBeforeCarrousel.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+              let lastElementFocusableBeforeCarrousel = allElementsFocusableBeforeCarrousel[allElementsFocusableBeforeCarrousel.length-1];
+              lastElementFocusableBeforeCarrousel.focus();
+            }
+          });
+
+          nextEl.addEventListener("keydown", (event) => {
+            if (
+              event.key === "Tab" &&
+              event.shiftKey
+            ) {
+              event.preventDefault();
+              let titreTuileActive = arrSlide[getIndexDerniereTuileVisible()].querySelector(".ds44-card__title a[href]:not([disabled])");
+              titreTuileActive.focus();
+            }
+          });
+
+          for(let tuile of arrSlide) {
+            var focusable = tuile.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+            var firstFocusable = focusable[0];
+            firstFocusable.addEventListener("keydown", (event) => {
+              if (
+                tuile === arrSlide[swiperObj.activeIndex] &&
+                event.key === "Tab" &&
+                event.shiftKey
+              ) {
+                event.preventDefault();
+                prevEl.focus();
+              }
+            });
+            var lastFocusable = focusable[focusable.length - 1];
+            firstFocusable.addEventListener("keydown", (event) => {
+              if (
+                tuile === arrSlide[getIndexDerniereTuileVisible()] &&
+                event.key === "Tab" &&
+                ! event.shiftKey
+              ) {
+                event.preventDefault();
+                nextEl.focus();
+              }
+            });
+          }
+
         } else {
 
           disableAllTabIndexes(element);
-          arrSlide.forEach((slide, index) => {
-            slide.querySelector(".ds44-card__title a[href]:not([disabled])").setAttribute("tabindex", index)
-          });
+          let index = 0
+          for(let slide of arrSlide) {
+            slide.querySelector(".ds44-card__title a[href]:not([disabled])").setAttribute("tabindex", index);
+            index++;
+          }
 
         }
         let spanNotif = element.getElementsByClassName("swiper-notification")[0];
@@ -109,28 +198,49 @@ class Carrousel {
       });
     }
 
-    // besoin de garder les classes pour l'initialisation des caroussel avec js, donc on les enleve a la fin
+    // besoin de garder les classes pour l'initialisation des caroussel avec js, donc on les enleve qu'a la fin
     this.removeAffichageSansJs();
   }
 
-  getTitleCaroussel(parent, index) {
-    let title = null;
-    while(
-      parent != null &
-      title == null
-    ) {
-      for(var sibling of getSiblings(parent)) {
-        if(
-          sibling != null &&
-          sibling.classList.contains("h2-like")
+  updateVisibiliteTuiles(arrSlide, indexPremiereTuileVisible, indexDerniereTuileVisible) {
+
+    if(indexDerniereTuileVisible > indexPremiereTuileVisible) {
+      let index = 0
+      for(let slide of arrSlide) {
+        if (
+          index >= indexPremiereTuileVisible &&
+          index <= indexDerniereTuileVisible
         ) {
-          title = sibling;
-          break;
+          slide.removeAttribute("aria-hidden");
+          slide.style.visibility= "visible";
+        } else {
+          slide.setAttribute("aria-hidden", "true");
+          slide.style.visibility= "hidden";
         }
-      };
-      parent = parent.parentElement;
+        index++;
+      }
+    } else {
+      let index = 0
+      for(let slide of arrSlide) {
+        if (
+            (
+              index >= indexPremiereTuileVisible &&
+              index < arrSlide.length
+            ) ||
+            (
+              index <= indexDerniereTuileVisible &&
+              index >= 0
+            )
+        ) {
+          slide.removeAttribute("aria-hidden");
+          slide.removeAttribute("style");
+        } else {
+          slide.setAttribute("aria-hidden", "true");
+          slide.setAttribute("style", "display:none;");
+        }
+        index++;
+      }
     }
-    return title != null ? title.innerText : "Carrousel n°"+(index+1);
   }
 
   // enleve les classes css de l'affichage sans js
