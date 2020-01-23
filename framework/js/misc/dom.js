316b7429abe829b5f9ffc2018dcb60728085f621
xavier@altapixel.com <xavier@gibbox.com>
2020-01-23 17:01:35 +0100

    #46 - Enhance tooltip

--- framework/js/misc/dom.js
+++ framework/js/misc/dom.js
@@ -2,10 +2,32 @@
 
 class MiscDom {
     static getAttribute(element, attributeName, defautValue = null) {
-        if(!element || !attributeName) {
+        if (!element || !attributeName) {
             return defautValue;
         }
 
         return (element.hasAttribute(attributeName) === true ? element.getAttribute(attributeName) : defautValue);
+    }
+
+    static addClasses(element, classNames) {
+        if (typeof classNames === 'string') {
+            classNames = [classNames];
+        }
+
+        if (element.classList) {
+            for(let index in classNames) {
+                element.classList.add(classNames[index]); // IE 10+
+            }
+        } else {
+            element.className += ' ' + classNames.join(' '); // IE 8+
+        }
+    }
+
+    static hasClass(element, className) {
+        if (element.classList) {
+            return element.classList.contains(className); // IE 10+
+        } else {
+            return new RegExp('(^| )' + className + '( |$)', 'gi').test(element.className); // IE 8+ ?
+        }
     }
 }
--- framework/js/tooltip/abstract.js
+++ framework/js/tooltip/abstract.js
@@ -1,8 +1,5 @@
-'use strict';
-
 class Tooltip {
     constructor() {
-        // Variables
         this.TOOLTIP_SIMPLE = 'js-simple-tooltip';
         this.TOOLTIP_SIMPLE_CONTAINER = 'simpletooltip_container';
         this.TOOLTIP_SIMPLE_RAW = 'simpletooltip';
@@ -10,69 +7,95 @@ class Tooltip {
         this.TOOLTIP_DATA_TEXT = 'data-simpletooltip-text';
         this.TOOLTIP_DATA_PREFIX_CLASS = 'data-simpletooltip-prefix-class';
         this.TOOLTIP_DATA_CONTENT_ID = 'data-simpletooltip-content-id';
+        this.ATTR_DESCRIBEDBY = 'aria-describedby';
+        this.ATTR_HIDDEN = 'aria-hidden';
+        this.ATTR_ROLE = 'role';
+        this.ROLE = 'tooltip';
+        this.DATA_HASH_ID = 'data-hashtooltip-id';
 
-        // Build tooltips in DOM
-        this.createTooltips();
+        // Create tooltips
+        document
+            .querySelectorAll('.' + this.TOOLTIP_SIMPLE)
+            .forEach(this.create.bind(this));
 
-        // Add events
+        // Bind events
         ['mouseenter', 'focus', 'mouseleave', 'blur', 'keydown']
-            .forEach(eventName => {
-                document.body.addEventListener(eventName, this.showHide.bind(this), true);
+            .forEach(eventType => {
+                document.body.addEventListener(eventType, this.showHide.bind(this), true);
             });
     }
 
-    createTooltips() {
-        document
-            .querySelectorAll('.' + this.TOOLTIP_SIMPLE)
-            .forEach((tooltipElement) => {
-                const randomId = Math.random().toString(32).slice(2, 12);
-                let text = MiscDom.getAttribute(tooltipElement, this.TOOLTIP_DATA_TEXT, '');
-                const prefixClassName = MiscDom.getAttribute(tooltipElement, this.TOOLTIP_DATA_PREFIX_CLASS, '');
-                const contentId = MiscDom.getAttribute(tooltipElement, this.TOOLTIP_DATA_CONTENT_ID, '');
-                const id = this.TOOLTIP_SIMPLE_LABEL_ID + randomId;
+    create(element) {
+        const hashId = Math.random().toString(32).slice(2, 12);
+        const prefixClassName = MiscDom.getAttribute(element, this.TOOLTIP_DATA_PREFIX_CLASS);
+        const contentId = MiscDom.getAttribute(element, this.TOOLTIP_DATA_CONTENT_ID);
 
-                // Attach the tooltip position
-                tooltipElement.setAttribute('aria-describedby', id);
+        // Set tooltip attributes
+        element.setAttribute(this.DATA_HASH_ID, hashId);
+        element.setAttribute(this.ATTR_DESCRIBEDBY, this.TOOLTIP_SIMPLE_LABEL_ID + hashId);
 
-                // Create wrapper
-                const wrapperClassName = [prefixClassName, this.TOOLTIP_SIMPLE_CONTAINER].filter(Boolean).join('-');
-                const wrapper = document.createElement('span');
-                wrapper.classList.add(wrapperClassName);
-                tooltipElement.parentNode.insertBefore(wrapper, tooltipElement);
-                wrapper.appendChild(tooltipElement);
+        // Put the tooltip in a wrapper
+        const wrapperClassName = [prefixClassName, this.TOOLTIP_SIMPLE_CONTAINER].filter(Boolean).join('-');
+        const wrapper = document.createElement('span');
+        MiscDom.addClasses(wrapper, wrapperClassName);
+        wrapper.setAttribute(this.DATA_HASH_ID, hashId);
+        element.parentNode.insertBefore(wrapper, element);
+        wrapper.appendChild(element);
 
-                const className = [prefixClassName, this.TOOLTIP_SIMPLE_RAW].filter(Boolean).join('-');
-                // If there is no content but an id we try to fetch dat content id
-                if (!text && contentId) {
-                    const contentFromId = document.getElementById(contentId);
-                    if (contentFromId) {
-                        text = contentFromId.innerHTML;
-                    }
-                }
+        // Determine the tooltip bubble content
+        let content = MiscDom.getAttribute(element, this.TOOLTIP_DATA_TEXT);
+        if (!content && contentId) {
+            const contentFromId = document.getElementById(contentId);
+            if (contentFromId) {
+                content = contentFromId.innerHTML;
+            }
+        }
 
-                wrapper.insertAdjacentHTML(
-                    'beforeend',
-                    '<span class="' + className + ' ' + this.TOOLTIP_SIMPLE + '" id="' + id + '" role="tooltip" aria-hidden="true">' + text + '</span>'
-                );
-            });
+        // Create tooltip bubble
+        const bubbleClassName = [prefixClassName, this.TOOLTIP_SIMPLE_RAW].filter(Boolean).join('-');
+        const bubble = document.createElement('span');
+        bubble.setAttribute('id', this.TOOLTIP_SIMPLE_LABEL_ID + hashId);
+        MiscDom.addClasses(bubble, [bubbleClassName, this.TOOLTIP_SIMPLE]);
+        bubble.setAttribute(this.ATTR_ROLE, this.ROLE);
+        bubble.setAttribute(this.ATTR_HIDDEN, 'true');
+        bubble.setAttribute(this.DATA_HASH_ID, hashId);
+        bubble.innerHTML = content;
+
+        wrapper.appendChild(bubble);
     }
 
     showHide(evt) {
-        if (evt.target.classList.contains(this.TOOLTIP_SIMPLE) === true) {
-            let tooltipLauncher = evt.target;
-            let item = document.getElementById(tooltipLauncher.getAttribute('aria-describedby'));
-            if (!item) {
-                return;
-            }
+        const element = evt.target;
 
-            const eventType = evt.type;
-            if (eventType === 'mouseenter' || eventType === 'focus') {
-                // Show
-                item.setAttribute('aria-hidden', 'false');
-            } else if (eventType === 'mouseleave' || eventType === 'blur' || (eventType === 'keydown' && evt.keyCode === 27)) {
-                // Hide
-                item.setAttribute('aria-hidden', 'true');
-            }
+        // Determine the tooltip button and its corresponding event types
+        let tooltipButton = null;
+        let showEventType = null;
+        let hideEventType = null;
+        if (MiscDom.hasClass(element, this.TOOLTIP_SIMPLE) === true) {
+            tooltipButton = element;
+            showEventType = 'focus';
+            hideEventType = 'blur';
+        } else if(MiscDom.hasClass(element, this.TOOLTIP_SIMPLE_CONTAINER) === true) {
+            tooltipButton = element.querySelector('[' + this.ATTR_DESCRIBEDBY + ']');
+            showEventType = 'mouseenter';
+            hideEventType = 'mouseleave';
+        }
+        if (!tooltipButton) {
+            return;
+        }
+
+        // Get the corresponding tooltip bubble
+        const tooltipBubble = document.getElementById(MiscDom.getAttribute(tooltipButton, this.ATTR_DESCRIBEDBY));
+        if (!tooltipBubble) {
+            return;
+        }
+
+        if (evt.type === showEventType) {
+            // Show tooltip bubble
+            tooltipBubble.setAttribute(this.ATTR_HIDDEN, 'false');
+        } else if (evt.type === hideEventType || (evt.type === 'keydown' && evt.keyCode === 27)) {
+            // Hide tooltip bubble
+            tooltipBubble.setAttribute(this.ATTR_HIDDEN, 'true');
         }
     }
 }
