62e457965f42b1a3b76b104bbda7cfb9632c1093
DAVID Camille <camille.david@loire-atlantique.fr>
2020-01-23 14:03:34 +0100

    FIX js pull request

--- framework/js/components/onglet.js
+++ framework/js/components/onglet.js
@@ -4,10 +4,9 @@ class Onglet {
 
   constructor() {
     let allTabs = document.querySelectorAll('.js-tablist__link');
-    if (!(allTabs == null)) {
-      allTabs.forEach((tab) => {
-        tab.addEventListener('click', () => this.processTransitionOnglets(tab, allTabs));
-        tab.addEventListener('keypress', (event) => fusionneKeyPressedWithClicked(event));
+    allTabs.forEach((tab) => {
+        tab.addEventListener('click', this.processTransitionOnglets.bind(this, tab, allTabs));
+        tab.addEventListener('keypress', window.fusionneKeyPressedWithClicked);
 
         if(tab.hasAttribute('aria-current')) {
           tab.classList.add('ds44-tabs__linkSelected');
@@ -15,33 +14,38 @@ class Onglet {
 
         let hrefTag = this.getTagFromHref(tab.getAttribute('href'));
 
-        let tabpanel = document.querySelector(hrefTag);
-        let tabpanelExit = tabpanel.children[tabpanel.children.length-1];
+        let tabPanel = document.querySelector(hrefTag);
+        if(!tabPanel) {
 
-        tabpanelExit.addEventListener('click', (event) => {
+            return;
+        }
+        let tabPanelExit = tabPanel.children[tabPanel.children.length-1];
+
+        tabPanelExit.addEventListener('click', (event) => {
           event.preventDefault();
           tab.focus();
 
           let header = document.querySelector('header .ds44-header');
-          if(header != null) {
-            let distanceToScroll = header.offsetHeight;
-            let distanceEntreTabPanelExitEtTab = getPositionY(tabpanelExit) - getPositionY(tab) + distanceToScroll;
-            let distanceEntreTabPanelExitEtHautEcran = getPositionY(tabpanelExit) - document.scrollingElement.scrollTop;
-            if(distanceEntreTabPanelExitEtTab >= distanceEntreTabPanelExitEtHautEcran) { //si le tab est au dessus de l'ecran visible
+          if(!header) {
+
+              return;
+          }
+          let distanceToScroll = header.offsetHeight;
+          let distanceEntreTabPanelExitEtTab = getPositionY(tabPanelExit) - getPositionY(tab) + distanceToScroll;
+          let distanceEntreTabPanelExitEtHautEcran = getPositionY(tabPanelExit) - document.scrollingElement.scrollTop;
+          if(distanceEntreTabPanelExitEtTab >= distanceEntreTabPanelExitEtHautEcran) { //si le tab est au dessus de l'ecran visible
               document.scrollingElement.scrollBy(0, -distanceToScroll * 2);
-            }
           }
         });
-        tabpanelExit.addEventListener('keypress', (event) => fusionneKeyPressedWithClicked(event));
+        tabPanelExit.addEventListener('keypress', window.fusionneKeyPressedWithClicked);
 
         if(tab.getAttribute('aria-current') === 'true') {
           tab.focus();
-          tabpanel.style['display'] = 'block';
-          timerClass(tabpanel, 'opacity', '1', 150);
+          tabPanel.style['display'] = 'block';
+          window.timerClass(tabPanel, 'opacity', '1', 150);
         }
 
-      });
-    }
+    });
   }
 
   // effectue une transition des display:none sur les contenus des onglets
@@ -51,22 +55,27 @@ class Onglet {
 
       let hrefTag = this.getTagFromHref(tab.getAttribute('href'));
 
-      let tabpanel = document.querySelector(hrefTag);
+      let tabPanel = document.querySelector(hrefTag);
+
+      if(!tabPanel) {
+
+          return;
+      }
 
       if (tabClicked === tab) {
         tab.classList.add('ds44-tabs__linkSelected');
 
-        timerClass(tabpanel, 'display', 'block', 150);
-        timerClass(tabpanel, 'opacity', '1', 300);
+        timerClass(tabPanel, 'display', 'block', 150);
+        timerClass(tabPanel, 'opacity', '1', 300);
 
-        let tabpanelAnchor = tabpanel.children[0];
-        tabpanelAnchor.focus();
+        let tabPanelAnchor = tabPanel.children[0];
+        tabPanelAnchor.focus();
 
       } else {
         tab.classList.remove('ds44-tabs__linkSelected');
 
-        tabpanel.style['opacity'] = '0';
-        timerDisplayNone(tabpanel, 150);
+        tabPanel.style['opacity'] = 0;
+        window.timerDisplayNone(tabPanel, 150);
       }
 
     });
@@ -74,9 +83,11 @@ class Onglet {
 
   // Récupère le tag ID d'un HREF, s'il existe
   getTagFromHref(href) {
-    if (href.indexOf('#') != -1) {
+    if (href.indexOf('#') !== -1) {
+
       return href.slice(href.indexOf('#'));
     }
+
     return '#';
   }
 
