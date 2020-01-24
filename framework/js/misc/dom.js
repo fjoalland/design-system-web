02e67bd507205551629bd01a6db28ac0a1b4abfc
xavier@altapixel.com <xavier@gibbox.com>
2020-01-24 17:03:54 +0100

    \#46 - Unification du code

--- framework/js/misc/dom.js
+++ framework/js/misc/dom.js
@@ -2,10 +2,6 @@
 
 class MiscDom {
     static getAttribute(element, attributeName, defautValue = null) {
-        if (!element || !attributeName) {
-            return defautValue;
-        }
-
         return (element.hasAttribute(attributeName) === true ? element.getAttribute(attributeName) : defautValue);
     }
 
