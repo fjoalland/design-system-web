fa99d2728484ec19f0b8c453959ef25046590c6e
LChoquet <lucas.choquet@trsb.net>
2020-01-24 15:15:08 +0100

    Refacto : prise en compte du review

--- framework/js/components/overlaySrcFocus.js
+++ framework/js/components/overlaySrcFocus.js
@@ -2,33 +2,31 @@
 
 class OverlaySrcFocus {
 
-	constructor() {
-		// Aouter un event listener sur les boutons qui ouvrent un overlay / modale
-		if (document.querySelector('*[data-open-overlay="true"]') !== null) {
-			document.querySelectorAll('*[data-open-overlay="true"]').forEach((element) => {
-                element.addEventListener('click', () => {
-                    this.designElementAsOverlaySrc(element);
-                })
-            });
-		}
+    constructor() {
+        // Ajouter un event listener sur les boutons qui ouvrent un overlay / modale
+        document.querySelectorAll('[data-open-overlay="true"]').forEach((element) => {
+            element.addEventListener('click', () => {
+                this.designElementAsOverlaySrc(element);
+            })
+        });
 
-		document.addEventListener('overlay:hide', () => { this.removeDesignatedOverlaySrc() });
-		document.addEventListener('modal:hide', () => { this.removeDesignatedOverlaySrc() });
-	}
+        document.addEventListener('overlay:hide', () => { this.removeDesignatedOverlaySrc() });
+        document.addEventListener('modal:hide', () => { this.removeDesignatedOverlaySrc() });
+    }
 
-	// Ajoute un attribut à un élément pour le désigner en tant que source d'une ouverture d'overlay
-	designElementAsOverlaySrc(element) {
-		if (element !== null) element.setAttribute("data-src-overlay","true");
-	}
+    // Ajoute un attribut à un élément pour le désigner en tant que source d'une ouverture d'overlay
+    designElementAsOverlaySrc(element) {
+        if (element !== null) element.setAttribute("data-src-overlay","true");
+    }
 
-	// Retire l'attribut désignant un élément comme source d'ouverture d'overlay et focus sur cet élément
-	removeDesignatedOverlaySrc() {
-		let itDesignatedElem = document.querySelector('*[data-src-overlay="true"]');
-		if (itDesignatedElem !== null) {
-			itDesignatedElem.removeAttribute("data-src-overlay");
-			itDesignatedElem.focus();
-		}
-	}
+    // Retire l'attribut désignant un élément comme source d'ouverture d'overlay et focus sur cet élément
+    removeDesignatedOverlaySrc() {
+        let itDesignatedElem = document.querySelector('[data-src-overlay="true"]');
+        if (itDesignatedElem !== null) {
+            itDesignatedElem.removeAttribute("data-src-overlay");
+            itDesignatedElem.focus();
+        }
+    }
 
 }
 
