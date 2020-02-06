1cbbde01b1afead01e4f72efc56f859a31e7797f
xavier@altapixel.com <xavier@gibbox.com>
2020-02-06 13:32:44 +0100

    RS-495 - Tooltip

--- framework/js/tooltip/abstract.js
+++ framework/js/tooltip/abstract.js
@@ -39,6 +39,7 @@ class Tooltip {
         const wrapper = document.createElement('span');
         MiscDom.addClasses(wrapper, wrapperClassName);
         wrapper.setAttribute(this.DATA_HASH_ID, hashId);
+        wrapper.setAttribute('role', 'presentation');
         element.parentNode.insertBefore(wrapper, element);
         wrapper.appendChild(element);
 
