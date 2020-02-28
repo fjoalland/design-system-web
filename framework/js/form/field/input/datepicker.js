595293b491db6de64674725d8856a8ea7392b803
xavier@altapixel.com <xavier@gibbox.com>
2020-02-28 18:14:52 +0100

    RS-711 - Revue du calendrier

--- framework/js/form/field/input/datepicker.js
+++ framework/js/form/field/input/datepicker.js
@@ -1,6 +1,9 @@
 class FormFieldInputDatepicker extends FormFieldInputAbstract {
     constructor() {
         super('.ds44-datepicker__shape', 'datepicker');
+
+        this.lastInputValue = null;
+        this.calendar = null;
     }
 
     create(element) {
@@ -22,6 +25,17 @@ class FormFieldInputDatepicker extends FormFieldInputAbstract {
             MiscEvent.addListener('focus', this.focus.bind(this, objectIndex), inputElement);
             MiscEvent.addListener('blur', this.blur.bind(this, objectIndex), inputElement);
         });
+
+        MiscEvent.addListener('keydown', this.keyDown.bind(this, objectIndex), object.inputElements[0]);
+        MiscEvent.addListener('keydown', this.keyDown.bind(this, objectIndex), object.inputElements[1]);
+        MiscEvent.addListener('keyup', this.keyUp.bind(this, objectIndex), object.inputElements[0]);
+        MiscEvent.addListener('keyup', this.keyUp.bind(this, objectIndex), object.inputElements[1]);
+        MiscEvent.addListener('click', this.focusOut.bind(this, objectIndex), document.body);
+
+        const calendarButton = MiscDom.getNextSibling(element, '.ds44-calendar');
+        if (calendarButton) {
+            MiscEvent.addListener('click', this.showHideCalendar.bind(this, objectIndex), calendarButton);
+        }
     }
 
     write(objectIndex) {
@@ -59,6 +73,8 @@ class FormFieldInputDatepicker extends FormFieldInputAbstract {
     }
 
     focus(objectIndex) {
+        this.lastInputValue = null;
+
         super.focus(objectIndex);
 
         const object = this.objects[objectIndex];
@@ -70,6 +86,8 @@ class FormFieldInputDatepicker extends FormFieldInputAbstract {
     }
 
     blur(objectIndex) {
+        this.lastInputValue = null;
+
         super.blur(objectIndex);
 
         if (!this.isEmpty(objectIndex)) {
@@ -82,6 +100,47 @@ class FormFieldInputDatepicker extends FormFieldInputAbstract {
         }
 
         object.textElement.classList.remove('show');
+    }
+
+    focusOut(objectIndex, evt) {
+        if(
+            !this.calendar ||
+            this.calendar.index !== objectIndex
+        ) {
+            return;
+        }
+
+        const object = this.objects[objectIndex];
+        if (
+            !evt ||
+            object.containerElement.contains(evt.target)
+        ) {
+            return;
+        }
+
+        this.hideCalendar();
+    }
+
+    keyDown(objectIndex, evt) {
+        this.lastInputValue = evt.currentTarget.value;
+    }
+
+    keyUp(objectIndex, evt) {
+        if (
+            !this.lastInputValue ||
+            this.lastInputValue.length !== 1 ||
+            evt.currentTarget.value.length !== 2
+        ) {
+            return;
+        }
+
+        // If two digits, go to next field
+        const object = this.objects[objectIndex];
+        if (evt.currentTarget === object.inputElements[0]) {
+            MiscAccessibility.setFocus(object.inputElements[1]);
+        } else {
+            MiscAccessibility.setFocus(object.inputElements[2]);
+        }
     }
 
     getText(objectIndex) {
@@ -139,6 +198,46 @@ class FormFieldInputDatepicker extends FormFieldInputAbstract {
                 'value': dateText
             }
         )
+    }
+
+    showHideCalendar(objectIndex) {
+        const object = this.objects[objectIndex];
+
+        let restart = !(this.calendar && this.calendar.id === object.id);
+        this.hideCalendar();
+        if (!restart) {
+            return;
+        }
+
+        this.calendar = {
+            'id': object.id,
+            'index': objectIndex,
+            'object': new CalendarStandard({
+                'element': object.containerElement.querySelector('.vanilla-calendar'),
+                'onSelect': this.selectDate.bind(this, objectIndex)
+            })
+        };
+    }
+
+    hideCalendar() {
+        if (this.calendar) {
+            this.calendar.object.destroy();
+            this.calendar = null;
+        }
+    }
+
+    selectDate(objectIndex, data) {
+        const object = this.objects[objectIndex];
+        const selectedData = new Date(data.date);
+        object.inputElements[0].value = (selectedData.getDate() + '').padStart(2, '0');
+        object.inputElements[1].value = ((selectedData.getMonth() + 1) + '').padStart(2, '0');
+        object.inputElements[2].value = (selectedData.getFullYear() + '').padStart(2, '0');
+
+        this.hideCalendar();
+        this.focusOnTextElement(objectIndex);
+        this.record(objectIndex);
+        this.showHideResetButton(objectIndex);
+        this.enableDisableLinkedField(objectIndex);
     }
 }
 
