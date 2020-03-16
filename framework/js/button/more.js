5edf031a4ac9b35ca447a02d917a15a3f6fbbbe0
xavier@altapixel.com <xavier@gibbox.com>
2020-03-16 19:08:14 +0100

    RS-803 - Bouton FAQ

---
+++ framework/js/button/more.js
@@ -0,0 +1,37 @@
+class ButtonMore {
+    constructor () {
+        this.nbResults = 5;
+
+        document
+            .querySelectorAll('.ds44-js-more-button')
+            .forEach((buttonElement) => {
+                MiscEvent.addListener('click', this.showMore.bind(this), buttonElement);
+            });
+    }
+
+    showMore (evt) {
+        const buttonElement = evt.currentTarget;
+        const collapserElement = MiscDom.getPreviousSibling(buttonElement, '.ds44-collapser');
+        if (!collapserElement) {
+            return;
+        }
+
+        const collapserItemElements = Array.prototype.slice.call(
+            collapserElement.querySelectorAll('.ds44-collapser_element.hidden'),
+            0,
+            this.nbResults
+        );
+        collapserItemElements
+            .forEach((collapserItemElement) => {
+                collapserItemElement.classList.remove('hidden');
+            });
+
+
+        if (!collapserElement.querySelector('.ds44-collapser_element.hidden')) {
+            buttonElement.classList.add('hidden');
+        }
+    }
+}
+
+// Singleton
+new ButtonMore();
