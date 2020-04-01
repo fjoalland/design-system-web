7f0ca7999640235b6a51154e1abec5868e7545fe
xavier@altapixel.com <xavier@gibbox.com>
2020-04-01 18:57:39 +0200

    RS-518 - Create search results

--- framework/js/map/abstract.js
+++ framework/js/map/abstract.js
@@ -1,6 +1,8 @@
 class MapAbstract {
     constructor (selector) {
         this.objects = [];
+        this.mapLanguageLoaded = false;
+        this.mapLoaded = false;
 
         document
             .querySelectorAll(selector)
@@ -14,19 +16,31 @@ class MapAbstract {
         document.head.appendChild(linkElement);
 
         let scriptElement = document.createElement('script');
+        scriptElement.setAttribute('src', 'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-language/v0.10.1/mapbox-gl-language.js');
+        scriptElement.setAttribute('type', 'text/javascript');
+        document.head.appendChild(scriptElement);
+        MiscEvent.addListener(
+            'load',
+            ((selector) => {
+                this.mapLanguageLoaded = true;
+
+                this.mapLoad(selector);
+            }).bind(this, selector),
+            scriptElement
+        );
+
+        scriptElement = document.createElement('script');
         scriptElement.setAttribute('src', 'https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.js');
         scriptElement.setAttribute('type', 'text/javascript');
         document.head.appendChild(scriptElement);
         MiscEvent.addListener(
             'load',
             ((selector) => {
+                this.mapLoaded = true;
+
                 window.mapboxgl.accessToken = 'pk.eyJ1IjoiemF6aWZmaWMiLCJhIjoiY2s3bmtxYXh2MDNqZzNkdDc3NzJ0aGdqayJ9.TuhsI1ZKXwKSGw2F3bVy5g';
 
-                document
-                    .querySelectorAll(selector)
-                    .forEach((element) => {
-                        this.load(element);
-                    });
+                this.mapLoad(selector);
             }).bind(this, selector),
             scriptElement
         );
@@ -43,6 +57,19 @@ class MapAbstract {
         this.objects.push(object);
     }
 
+    mapLoad (selector) {
+        if(
+            this.mapLanguageLoaded &&
+            this.mapLoaded
+        ) {
+            document
+                .querySelectorAll(selector)
+                .forEach((element) => {
+                    this.load(element);
+                });
+        }
+    }
+
     load (element) {
         for (let objectIndex = 0; objectIndex < this.objects.length; objectIndex++) {
             const object = this.objects[objectIndex];
@@ -55,8 +82,7 @@ class MapAbstract {
                     center: [-1.8157647, 47.2780468],
                     zoom: 8
                 });
-
-                this.afterLoad(objectIndex);
+                object.map.on('load', this.afterLoad.bind(this, objectIndex));
 
                 break;
             }
